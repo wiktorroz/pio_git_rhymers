94738046de90a3724c60dbd6a43e7f8a197c2997
Wiktor <245913@edu.p.lodz.pl>
2024-05-04 00:27:33 +0200

    2. poprawa formatowania kodu w klasie HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -9,9 +9,9 @@ int totalRejected = 0;
     }
 
     public void countIn(int in) {
-    if (!callCheck() && in > peekaboo())
+    	if (!callCheck() && in > peekaboo())
             totalRejected++;
-            else
-                super.countIn(in);
+        else
+            super.countIn(in);
     }
 }
