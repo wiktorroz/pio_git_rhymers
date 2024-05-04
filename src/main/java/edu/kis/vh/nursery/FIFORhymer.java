0c0a9d164c4de6be0f756bc5315ea31085c50053
Wiktor <245913@edu.p.lodz.pl>
2024-05-04 02:30:02 +0200

    5. Poprawiono błędy konwencji nazewniczej pól klas we wszystkich klasach (były już poprawione w 4. punkcie)

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,21 +1,21 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-    
-    @Override
-    public int countOut() {
-        while (!callCheck())
-            
-        temp.countIn(super.countOut());
-        
-        int ret = temp.countOut();
-        
-        while (!temp.callCheck())
-            
-        countIn(temp.countOut());
-        
-        return ret;
-    }
+	public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
+	@Override
+	public int countOut() {
+		while (!callCheck())
+
+			temp.countIn(super.countOut());
+
+		int ret = temp.countOut();
+
+		while (!temp.callCheck())
+
+			countIn(temp.countOut());
+
+		return ret;
+	}
 }
